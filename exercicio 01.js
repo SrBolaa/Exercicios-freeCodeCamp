96d1a153d093b366e78cdd0b8c547e89ad6b4de4
Gabriel de Jesus <70569928+SrBolaa@users.noreply.github.com>
2022-07-11 17:08:48 -0300

    Update and rename Capitalizar a primeira letra de cada palavra de uma frase.js to exercicio 01.js

--- exercicio 01.js
+++ exercicio 01.js
@@ -1,3 +1,5 @@
+//Capitalizar a primeira letra de cada palavra de uma frase
+
 /* Retorne a string fornecida com a primeira letra de cada palavra em letra maiúscula. Certifique-se de que o resto da palavra esteja em letras minúsculas.
 
 Para o propósito desse exercício, você também deve capitalizar as palavras conectoras como the e of. */
